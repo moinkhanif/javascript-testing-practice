178cd99f6b2b1890ab869a7713ec783ca1c8eb18
Ashah15 <ashaabdillahi53@gmail.com>
2020-10-14 01:39:12 +0300

    create the calculator.test.js for testing

--- calculator.js
+++ calculator.js
@@ -1,16 +1,38 @@
-const calculator = () => {
-	const subtract = (x, y) => x - y;
+// const calculator = () => {
+// 	const subtract = (x, y) => x - y;
 
-	const multiply = (x, y) => x * y;
+// 	const multiply = (x, y) => x * y;
 
-	const add = (x, y) => x + y;
+// 	const add = (x, y) => x + y;
 
-	const divide = (x, y) => x / y;
+// 	const divide = (x, y) => x / y;
 
-	return {subtrsct, multiply, add, divide};
+// 	return {subtrsct, multiply, add, divide};
+// };
+
+
+// calculator(8, 6);
+
+// module.exports = calculator;
+
+const calculator = {
+
+ add: (x, y) => {
+  return x + y;
+},
+
+ subtract: (x, y) => {
+  return x - y;
+},
+
+ multiply: (x, y) => {
+  return x * y;
+},
+
+ divide: (x, y) => {
+  return x / y;
+},
+
 };
-
-
-calculator(8, 6);
 
 module.exports = calculator;
