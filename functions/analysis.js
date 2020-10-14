cc55e647c5c07d86a35493539e64f4f4e94eb000
Ashah15 <ashaabdillahi53@gmail.com>
2020-10-14 23:26:51 +0300

    fixed eslint errors in analysis.js

--- functions/analysis.js
+++ functions/analysis.js
@@ -1,20 +1,13 @@
 const analysis = (arr) => {
   const sum = arr.reduce((num, val) => num + val, 0);
-  //  length: arr,
-  //  average: sum / length,
 
-  //  max: Math.max(...arr),
-  //  min: Math.min(...arr),
+  return {
+    length: arr.length,
+    average: sum / arr.length,
 
-   return {
-   	 // sum: arr.reduce((num, val) => num + val, 0),
-     length: arr.length,
-     average: sum / arr.length,
-
-     max: Math.max(...arr),
-     min: Math.min(...arr),
-   }
-
+    max: Math.max(...arr),
+    min: Math.min(...arr),
+  };
 };
 
 
