348a698326c299421dba6ab4f26553e7c9c8636a
Moin <kmoin786@gmail.com>
2020-10-14 22:58:08 +0530

    build caesar functions using given description and TDD

--- calculator.js
+++ calculator.js
@@ -1,37 +1,12 @@
-// const calculator = () => {
-// 	const subtract = (x, y) => x - y;
-
-// 	const multiply = (x, y) => x * y;
-
-// 	const add = (x, y) => x + y;
-
-// 	const divide = (x, y) => x / y;
-
-// 	return {subtrsct, multiply, add, divide};
-// };
-
-
-// calculator(8, 6);
-
-// module.exports = calculator;
-
 const calculator = {
 
- add: (x, y) => {
-  return x + y;
-},
+  add: (x, y) => x + y,
 
- subtract: (x, y) => {
-  return x - y;
-},
+  subtract: (x, y) => x - y,
 
- multiply: (x, y) => {
-  return x * y;
-},
+  multiply: (x, y) => x * y,
 
- divide: (x, y) => {
-  return x / y;
-},
+  divide: (x, y) => x / y,
 
 };
 
